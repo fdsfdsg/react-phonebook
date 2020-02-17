43d502bd2e4cd5eeb889600fb3bcd6d4d95544e5
park sangbeom <smctkdqja@gmail.com>
2020-02-17 23:25:23 +0900

    localStorage 적용

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 - [react project에 ESLint와 Prittier 끼얹기](https://velog.io/@velopert/eslint-and-prettier-in-react)
 - [React와 Virtual DOM](https://www.youtube.com/watch?v=muc2ZF0QIO4&feature=youtu.be)
 - [전개구문](https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Spread_syntax)
-VSC extension중 reactjs code snippets 이라는 react개발할때 함수형컴포넌트나 클래스컴포넌트를 바로 만들어주는 플러그인이있다.
 
-사용법 : 
+VSC extension중 reactjs code snippets 이라는 react개발할때 함수형컴포넌트 클래스컴포넌트 틀을 바로 만들어주는 플러그인
 
 1. rcc 입력 => class component
-2. rsc 입력 => function component  
-***
+2. rsc 입력 => function component 
--- src/App.js
+++ src/App.js
@@ -3,84 +3,100 @@ import PhoneForm from './Components/PhoneForm';
 import PhoneInfoList from './Components/PhoneInfoList';
 
 class App extends Component {
-
   id = 3;
-
   state = {
     information: [
       {
-        id : 0,
-        name: "박상범",
-        phone: "010-4690-4164"
+        id: 0,
+        name: '박상범',
+        phone: '010-4690-4164',
       },
       {
-        id : 1,
-        name: "아이유",
-        phone: "010-1234-5678"
+        id: 1,
+        name: '아이유',
+        phone: '010-1234-5678',
       },
       {
-        id : 2,
-        name: "아이린",
-        phone: "010-0123-0123"
+        id: 2,
+        name: '아이린',
+        phone: '010-0123-0123',
       },
     ],
     keyword: '',
+  };
+
+  UNSAFE_componentWillMount() {
+    const information = localStorage.information;
+    const id = localStorage.id;
+
+    if (information) {
+      this.setState({
+        information: JSON.parse(information),
+        id,
+      });
+    }
   }
 
-  handleChange = (e) => {
+  componentDidUpdate(prevProps, prevState) {
+    if (JSON.stringify(prevState.information) !== JSON.stringify(this.state.information)) {
+      localStorage.information = JSON.stringify(this.state.information)
+    }
+    if (prevState.id !== this.state.id) {
+      localStorage.id = this.state.id
+    }
+  }
+
+  handleChange = e => {
     this.setState({
       keyword: e.target.value,
     });
-  }
+  };
 
-  handleCreate = (data) => {
+  handleCreate = data => {
     const { information } = this.state;
     this.setState({
-      information : information.concat({
+      information: information.concat({
         ...data,
-        id: this.id++
-      })
+        id: this.id++,
+      }),
     });
-  }
+  };
 
-  handleRemove = (id) => {
-    const { information} = this.state;
+  handleRemove = id => {
+    const { information } = this.state;
     this.setState({
-      information: information.filter( 
-        info => info.id !== id)
+      information: information.filter(info => info.id !== id),
     });
-  }
+  };
 
-  handleUpdate = (id,data) => {
-    const { information } =this.state;
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
     this.setState({
-      information: information.map(
-        info => {
-          if(info.id === id){
-            return {
-              id,
-              ...data,
-            }
-          }
-          return info
+      information: information.map(info => {
+        if (info.id === id) {
+          return {
+            id,
+            ...data,
+          };
         }
-      )
+        return info;
+      }),
     });
-  }
+  };
 
   render() {
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
-        <input 
+        <input
           value={this.state.keyword}
           onChange={this.handleChange}
           placeholder="검색.."
         />
-        <PhoneInfoList 
+        <PhoneInfoList
           data={this.state.information.filter(
-            info => info.name.indexOf(this.state.keyword) > -1
-          )} 
+            info => info.name.indexOf(this.state.keyword) > -1,
+          )}
           onRemove={this.handleRemove}
           onUpdate={this.handleUpdate}
         />
@@ -90,4 +106,3 @@ class App extends Component {
 }
 
 export default App;
-
